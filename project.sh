3641f78d831a94bce1e66522b50d1254e3e0196a
aljazmc <mlinaric.aljaz@gmail.com>
2026-09-10 12:05:13 +0200

    chore: update clean function

--- project.sh
+++ project.sh
@@ -5,18 +5,19 @@ clean() {
 find . -mindepth 2 -maxdepth 2 \
     | sed "
         /aports\/scripts/d;
-        /.\/.git/d;
+        /\.\/\.git/d;
     " \
     | xargs -I {} rm -rf {}
 
 find aports/scripts -mindepth 1 -maxdepth 1 \
     | sed "
+        /aports\/scripts\/genapkovl-test.sh/d;
         /aports\/scripts\/mkimg.test.sh/d;
     " \
     | xargs -I {} rm -rf {}
 
 rmdir .abuild
-
+rm -rf alpine.qcow2 iso
 }
 
 $1
