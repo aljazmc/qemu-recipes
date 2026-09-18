769c875c1698172eb1459c776d0d85557d5bd3c7
aljazmc <mlinaric.aljaz@gmail.com>
2026-09-18 22:58:47 +0200

    fix: profile test

--- aports/scripts/mkimg.test.sh
+++ aports/scripts/mkimg.test.sh
@@ -1,14 +1,16 @@
 profile_test() {
-	profile_standard
-	profile_realname="LAMP WordPress Server"
+	profile_virt
+	kernel_flavors="virt"
+	title="LAMP WordPress Server"
+	desc="LAMP WordPress Server"
 	profile_abbrev="test"
-	profile_packages="$profile_packages \
-		apache2 \
-		php83 php83-apache2 php83-mysqli php83-session php83-gd \
-		php83-curl php83-mbstring php83-xml php83-zip php83-opcache \
-		php83-json php83-openssl php83-phar php83-iconv php83-dom \
-		php83-simplexml php83-fileinfo \
-		mariadb mariadb-client \
+	apks="$apks
+		apache2
+		php83 php83-apache2 php83-mysqli php83-session php83-gd
+		php83-curl php83-mbstring php83-xml php83-zip php83-opcache
+		php83-json php83-openssl php83-phar php83-iconv php83-dom
+		php83-simplexml php83-fileinfo
+		mariadb mariadb-client
 		openssh wget unzip"
-	profile_apkovl="genapkovl-test.sh"
+	apkovl="aports/scripts/genapkovl-test.sh"
 }
