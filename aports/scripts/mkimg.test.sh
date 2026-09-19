d175a542b6fe436d892fda1ded558e06d3371477
aljazmc <mlinaric.aljaz@gmail.com>
2026-09-19 10:58:40 +0200

    style: indentation

--- aports/scripts/mkimg.test.sh
+++ aports/scripts/mkimg.test.sh
@@ -1,16 +1,17 @@
 profile_test() {
-	profile_virt
-	kernel_flavors="virt"
-	title="LAMP WordPress Server"
-	desc="LAMP WordPress Server"
-	profile_abbrev="test"
-	apks="$apks
-		apache2
-		php83 php83-apache2 php83-mysqli php83-session php83-gd
-		php83-curl php83-mbstring php83-xml php83-zip php83-opcache
-		php83-json php83-openssl php83-phar php83-iconv php83-dom
-		php83-simplexml php83-fileinfo
-		mariadb mariadb-client
-		openssh wget unzip"
-	apkovl="aports/scripts/genapkovl-test.sh"
+    profile_virt
+    kernel_flavors="virt"
+    title="LAMP WordPress Server"
+    desc="LAMP WordPress Server"
+    profile_abbrev="test"
+    apks="$apks
+        apache2
+        php83 php83-apache2 php83-mysqli php83-session php83-gd
+        php83-curl php83-mbstring php83-xml php83-zip php83-opcache
+        php83-json php83-openssl php83-phar php83-iconv php83-dom
+        php83-simplexml php83-fileinfo
+        mariadb mariadb-client
+        openssh wget unzip
+    "
+    apkovl="aports/scripts/genapkovl-test.sh"
 }
